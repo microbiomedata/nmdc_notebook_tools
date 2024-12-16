bbb7f862687009bc89cfaa4b5d013aebcb103c20
olivia.hess <olivia.hess@pnnl.gov>
2024-12-16 11:13:49 -0500

    adding support for auto documentation

---
+++ src/nmdc_notebook_tools/docs/conf.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Add these lines to conf.py
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))  # Path to your project root
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "nmdc_notebook_tools"
+copyright = "2024, Olivia Hess"
+author = "Olivia Hess"
+release = "3/1/2025"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # For Google/NumPy style docstrings
+    "sphinx.ext.viewcode",  # Add links to source code
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
--- src/nmdc_notebook_tools/find.py
+++ src/nmdc_notebook_tools/find.py
@@ -1,63 +1,63 @@
 # -*- coding: utf-8 -*-
-from api import NMDClient
+from nmdc_notebook_tools.api import NMDClient
 import requests
 import urllib.parse
-from data_processing import convert_to_df
-from utils import get_id_list, split_list, string_mongo_list
+from nmdc_notebook_tools.data_processing import DataProcessing
+from nmdc_notebook_tools.utils import Utils
 
 
-def find_study_by_id(study_id: str):
-    """
-    Get a study from the NMDC API by its id.
-    params:
-        study_id: str
-            The id of the study to query.
-    """
-    api_client = NMDClient()
+class Find:
+    def __init__(self):
+        pass
 
-    url = f"{api_client.base_url}/study/{study_id}"
-    # get the reponse
-    response = requests.get(url)
-    # check it came back with OK
-    if response.status_code != 200:
-        return (response.status_code, "There was an error.")
-    results = response.json()
+    def find_study_by_id(self, study_id: str):
+        """
+        Get a study from the NMDC API by its id.
+        params:
+            study_id: str
+                The id of the study to query.
+        """
+        api_client = NMDClient()
 
-    return convert_to_df(results)
+        url = f"{api_client.base_url}/study/{study_id}"
+        # get the reponse
+        response = requests.get(url)
+        # check it came back with OK
+        if response.status_code != 200:
+            return (response.status_code, "There was an error.")
+        results = response.json()
 
+        return DataProcessing.convert_to_df(results)
 
-def _combine_results(resp: dict) -> dict:
-    pass
+    def find_study_by_attribute(self, attribute_name, attribute_value, page_size=25):
+        """
+        Get a study from the NMDC API by its name. Studies can be filtered based on their attributes found https://microbiomedata.github.io/nmdc-schema/Study/.
+        params:
+            study_name: str
+                The name of the study to query.
+            attribute_name: str
+                The name of the attribute to filter by.
+            attribute_value: str
+                The value of the attribute to filter by.
+            page_size: int
+                The number of results to return per page. Default is 25.
+        """
+        api_client = NMDClient()
 
+        filter = f"{attribute_name}.search:{attribute_value}"
 
-def find_study_by_attribute(attribute_name, attribute_value):
-    """
-    Get a study from the NMDC API by its name. Studies can be filter based on their attributes found https://microbiomedata.github.io/nmdc-schema/Study/.
-    params:
-        study_name: str
-            The name of the study to query.
-        attribute_name: str
-            The name of the attribute to filter by.
-        attribute_value: str
-            The value of the attribute to filter by.
-    """
-    api_client = NMDClient()
+        # Encode the filter for use in the URL
+        encoded_filter = urllib.parse.quote(filter)
 
-    filter = f"{attribute_name}.search:{attribute_value}"
-
-    # Encode the filter for use in the URL
-    encoded_filter = urllib.parse.quote(filter)
-
-    url = f"{api_client.base_url}/studies?filter={encoded_filter}"
-    # get the reponse
-    response = requests.get(url)
-    # check it came back with OK
-    if response.status_code != 200:
-        return (response.status_code, "There was an error.")
-    results = response.json()["results"]
-
-    return convert_to_df(results)
+        url = f"{api_client.base_url}/studies?filter={encoded_filter}&page_size={page_size}"
+        # get the reponse
+        response = requests.get(url)
+        # check it came back with OK
+        if response.status_code != 200:
+            return (response.status_code, "There was an error.")
+        results = response.json()["results"]
+        return DataProcessing.convert_to_df(results)
 
 
 if __name__ == "__main__":
-    find_study_by_attribute("id", "nmdc:sty-11-8fb6t785")
+    Find.find_study_by_attribute("id", "nmdc:sty")
