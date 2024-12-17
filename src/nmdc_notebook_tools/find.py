4f8a4cd21424738259cb3a7f02caa03898752979
olivia.hess <olivia.hess@pnnl.gov>
2024-12-17 16:30:30 -0500

    adding some specific find functions

--- src/nmdc_notebook_tools/find.py
+++ src/nmdc_notebook_tools/find.py
@@ -2,6 +2,8 @@
 from api import NMDClient
 import requests
 import urllib.parse
+from typing import List, Dict
+import json
 from data_processing import DataProcessing
 from utils import Utils
 
@@ -10,7 +12,7 @@ class Find:
     def __init__(self):
         pass
 
-    def find_study_by_id(self, study_id: str):
+    def find_study_by_id(self, study_id: str) -> List[Dict]:
         """
         Get a study from the NMDC API by its id.
         params:
@@ -25,11 +27,12 @@ class Find:
         # check it came back with OK
         if response.status_code != 200:
             return (response.status_code, "There was an error.")
-        results = response.json()
+        results = response.json()["results"]
+        return results
 
-        return dp.convert_to_df(results)
-
-    def find_study_by_attribute(self, attribute_name, attribute_value, page_size=25):
+    def find_study_by_attribute(
+        self, attribute_name, attribute_value, page_size=25
+    ) -> List[Dict]:
         """
         Get a study from the NMDC API by its name. Studies can be filtered based on their attributes found https://microbiomedata.github.io/nmdc-schema/Study/.
         params:
@@ -43,7 +46,6 @@ class Find:
                 The number of results to return per page. Default is 25.
         """
         api_client = NMDClient()
-        dp = DataProcessing()
         filter = f"{attribute_name}.search:{attribute_value}"
 
         # Encode the filter for use in the URL
@@ -56,12 +58,56 @@ class Find:
         if response.status_code != 200:
             return (response.status_code, "There was an error.")
         results = response.json()["results"]
-        return dp.convert_to_df(results)
+        return results
 
+    def find_study_by_filter(self, filter, page_size=25) -> List[Dict]:
+        """
+        Get a study from the NMDC API by its name. Studies can be filtered based on their attributes found https://microbiomedata.github.io/nmdc-schema/Study/.
+        params:
+            filter: str
+                The filter to use to query the studies.
+                Example: id:my_id, name:my_study, description:my_description
+
+            page_size: int
+                The number of results to return per page. Default is 25.
+        """
+        api_client = NMDClient()
+
+        url = (
+            f"{api_client.base_url}/studies?&filter={filter}&max_page_size={page_size}"
+        )
+        # get the reponse
+        response = requests.get(url)
+        # check it came back with OK
+        if response.status_code != 200:
+            return (response.status_code, "There was an error.")
+        results = response.json()["results"]
+        return results
+
+    def find_study_by_pi(self, pi_name: str, page_size=25) -> List[Dict]:
+        """
+        Get a study from the NMDC API by its name. Studies can be filtered based on their attributes found https://microbiomedata.github.io/nmdc-schema/Study/.
+        params:
+            pi_name: str
+                The name of the PI to filter by.
+            page_size: int
+                The number of results to return per page. Default is 25.
+        """
+        api_client = NMDClient()
+        filter = f"principal_investigator.has_raw_value.search:{pi_name}"
+
+        url = f"{api_client.base_url}/studies?filter={filter}&page_size={page_size}"
+        # get the reponse
+        response = requests.get(url)
+        # check it came back with OK
+        if response.status_code != 200:
+            return (response.status_code, "There was an error.")
+        results = response.json()["results"]
+        return results
+
+
+# function to filter the list of dicts?
+# can we project on study endpoint as well?
 
 if __name__ == "__main__":
     find = Find()
-    studies = find.find_study_by_attribute(
-        attribute_name="id", attribute_value="nmdc:sty"
-    )
-    print(studies)
