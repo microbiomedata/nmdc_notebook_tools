a2d92ed28610f33c2b5ff05678dbaa9633176ed2
olivia.hess <olivia.hess@pnnl.gov>
2024-12-19 10:07:04 -0500

    path update

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))  # Path to your project root
+sys.path.insert(0, os.path.abspath(".."))  # Path to your project root
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
