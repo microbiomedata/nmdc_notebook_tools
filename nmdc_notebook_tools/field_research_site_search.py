bf2f9fa118bc0b8cbbeef7ef4328d6e74b7ed786
olivia.hess <olivia.hess@pnnl.gov>
2025-01-21 16:48:05 -0500

    the only other collection with lat_long

--- nmdc_notebook_tools/field_research_site_search.py
+++ nmdc_notebook_tools/field_research_site_search.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from nmdc_notebook_tools.collection_search import CollectionSearch
+from nmdc_notebook_tools.lat_long_filters import LatLongFilters
 import logging
 
 logger = logging.getLogger(__name__)
 # TODO - what are these
 
 
-class FieldResearchSiteSearch(CollectionSearch):
+class FieldResearchSiteSearch(LatLongFilters, CollectionSearch):
     """
     Class to interact with the NMDC API to get field research site sets.
     """
