3802d3ba0fd4f04ba5f281d5666fe75317df1f0a
olivia.hess <olivia.hess@pnnl.gov>
2025-01-28 12:07:30 -0500

    minor updates, doc update, preparing for upload to pypi

--- docs/usage.rst
+++ docs/usage.rst
@@ -3,11 +3,6 @@ NMDC Notebook Tools Usage Notes
 
 Welcome to NMDC Notebook Tools usage notes. Here you will find helpful information on how to use the tools provided by this package.
 
-This module provides two different forms of filtering:
-
-1. `study` module: Filters using a comma-separated format.
-2. `collection` module: Filters in a MongoDB-like fashion.
-
-Refer to the respective module documentation for more details:
-- `study` module: :mod:`study`
-- `collection` module: :mod:`collection`
+Filtering should be formatted in MongoDB query syntax. The function build_filter in the DataProcessing class can be used to help build filters. Additonally, there are available functions in the CollectionSearch class that can be used to filter data without having to pass in a specific filter.
+The CollectionSearch class is a foundational component that defines common behaviors and properties between collections. Each subclass is designed to be more user-friendly and specific for certain collections, making them the recommended entry points for using the package. Each function of CollectionSearch can be accessed via each subclass.
+The subclasses will prefill necessary information for the user, such as the collection name. This will allow the user to focus on the data they are interested in, rather than the specifics of the collection.
--- nmdc_notebook_tools/data_processing.py
+++ nmdc_notebook_tools/data_processing.py
@@ -33,34 +33,66 @@ class DataProcessing:
         df.columns = new_col_names
         return df
 
-    def merge_dataframes(self, df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
-        return pd.merge(df1, df2, on="common_column", how="inner")
+    def merge_dataframes(
+        self, column: str, df1: pd.DataFrame, df2: pd.DataFrame
+    ) -> pd.DataFrame:
+        """
+        Merge two dataframes.
+        params:
+            column: str
+                The column to merge on.
+            df1: pd.DataFrame
+                The first dataframe to merge.
+            df2: pd.DataFrame
+                The second dataframe to merge.
+        returns:
+            pd.DataFrame
+        """
+        return pd.merge(df1, df2, on=column, how="inner")
 
-    ## Define a merging function to join results
-    # This function merges new results with the previous results that were used for the new API request. It uses two keys from each result to match on. `df1`
-    # is the data frame whose matching `key1` value is a STRING. `df2` is the other data frame whose matching `key2` has either a string OR list as a value.
-    # df1_explode_list and df2_explode_list are optional lists of columns in either dataframe that need to be exploded because they are lists (this is because
-    # drop_duplicates cant take list input in any column). Note that each if statement includes dropping duplicates after merging as the dataframes are being
-    # exploded which creates many duplicate rows after merging takes place.
     def merge_df(
         self,
         df1,
         df2,
         key1: str,
         key2: str,
-        df1_explode_list=None,
-        df2_explode_list=None,
     ):
-        if df1_explode_list is not None:
-            # Explode the lists in the df (necessary for drop duplicates)
-            for list in df1_explode_list:
-                df1 = df1.explode(list)
-        if df2_explode_list is not None:
-            # Explode the lists in the df (necessary for drop duplicates)
-            for list in df2_explode_list:
-                df2 = df2.explode(list)
+        """
+        Define a merging function to join results
+        This function merges new results with the previous results that were used for the new API request. It uses two keys from each result to match on.
+        params:
+            df1 and df2 are the two dataframes that need to be merged.
+            key1 is the column name in df1 that will be used to match with `key2` in `df2`.
+
+        This function automatically identifies columns that need to be exploded because they contain list-like elements, as drop_duplicates can't handle list elements.
+        """
+
+        def identify_and_explode(df):
+            for col in df.columns:
+                if any(isinstance(item, list) for item in df[col]):
+                    df = df.explode(col)
+            return df
+
+        df1 = identify_and_explode(df1)
+        df2 = identify_and_explode(df2)
+
         # Merge dataframes
         merged_df = pd.merge(df1, df2, left_on=key1, right_on=key2)
         # Drop any duplicated rows
         merged_df.drop_duplicates(keep="first", inplace=True)
         return merged_df
+
+    def build_filter(self, attributes):
+        """
+        Create a MongoDB filter using $regex for each attribute in the input dictionary. For nested attributes, use dot notation.
+
+        Parameters:
+        attributes (dict): Dictionary of attribute names and their corresponding values to match using regex.
+            Example: {"name": "example", "description": "example", "geo_loc_name": "example"}
+        Returns:
+        dict: A MongoDB filter dictionary.
+        """
+        filter_dict = {}
+        for attribute_name, attribute_value in attributes.items():
+            filter_dict[attribute_name] = {"$regex": attribute_value}
+        return self.string_mongo_list(filter_dict)
